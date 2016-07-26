925703297c2f0432b9e4a46d75dcc8bbd394e281
Maxim Zaslavsky <maxim@maximzaslavsky.com>
2016-07-26 14:44:34 -0400

    docker does not like the symlinks

--- custom/run.sh
+++ custom/run.sh
@@ -1,8 +1,8 @@
 rm -r /mnt/disks/mz/rnatmp;
 mkdir /mnt/disks/mz/rnatmp;
-ln -s ~/rna/ERR431606_1.fastq.gz /mnt/disks/mz/rnatmp/
-ln -s ~/rna/ERR431606_2.fastq.gz /mnt/disks/mz/rnatmp/
-ln -s ~/rna/b37.kallisto.idx /mnt/disks/mz/rnatmp/
+cp ~/rna/ERR431606_1.fastq.gz /mnt/disks/mz/rnatmp/
+cp ~/rna/ERR431606_2.fastq.gz /mnt/disks/mz/rnatmp/
+cp ~/rna/b37.kallisto.idx /mnt/disks/mz/rnatmp/
 echo 'try: '
 echo 'process b37.kallisto.idx testout'
 docker run --rm -it -v /mnt/disks/mz/rnatmp:/working maximz/rnapipeline:latest bash
