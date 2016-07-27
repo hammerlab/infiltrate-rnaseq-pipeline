6b9e7557663718212f507231613413592037459c
Maxim Zaslavsky <maxim@maximzaslavsky.com>
2016-07-27 17:38:17 -0400

    test cases

---
+++ get_data/test.sh
@@ -0,0 +1,16 @@
+sudo rm -rf /mnt/disks/mz/rnatmp2;
+mkdir /mnt/disks/mz/rnatmp2;
+
+# this should work properly
+docker run --rm -v /mnt/disks/mz/rnatmp2:/working maximz/download_rna:latest get_data quanta.cloudapp.net/test.txt.gz test.txt.gz /working;
+echo $?;
+
+# this should fail (file does not exist)
+docker run --rm -v /mnt/disks/mz/rnatmp2:/working maximz/download_rna:latest get_data quanta.cloudapp.net/test2.txt.gz test2.txt.gz /working;
+echo $?;
+
+# this should fail (broken archive)
+docker run --rm -v /mnt/disks/mz/rnatmp2:/working maximz/download_rna:latest get_data quanta.cloudapp.net/broken.gz broken.gz /working;
+echo $?;
+
+echo 'expected 0 1 1'
