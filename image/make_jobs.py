70369061cd7a100c864b7d026b02ebd8df10a695
Maxim Zaslavsky <maxim@maximzaslavsky.com>
2016-07-29 13:27:46 -0400

    more correct jobs

--- image/make_jobs.py
+++ image/make_jobs.py
@@ -3,14 +3,22 @@ import ntpath
 with open('template.yaml', 'r') as t:
 	template = t.read()
 
+samples = [] # e.g. ERR431566
+
 with open('../list_of_data.txt', 'r') as f:
 	for line in f:
 		line = line.strip()
 		filename = ntpath.basename(line)
-		trimmed_name = filename[:filename.index('.')]
-		with open('jobs/%s.yaml' % filename, 'w') as w:
-			w.write(template.format(**{
-				'filename': trimmed_name, # this is a prefix
-				'outputname': trimmed_name,
-				'jobid': trimmed_name.lower().replace('_','-')
-				}))
+		trimmed_name = filename[:filename.index('_')] # e.g. ERR431566
+		samples.append(trimmed_name)
+		assert len(trimmed_name) == len('ERR431566')
+
+samples = list(set(samples)) # dedupe
+
+for trimmed_name in samples:
+	with open('jobs/%s.yaml' % trimmed_name, 'w') as w:
+		w.write(template.format(**{
+			'filename': trimmed_name, # this is a prefix
+			'outputname': trimmed_name,
+			'jobid': trimmed_name.lower().replace('_','-') # note though with this trimming we won't have underscores, but just to be safe
+			}))
